68537657dce8369a50bd630828b6d4822d646590
Andrey <lyp1noff@gmail.com>
2019-08-13 00:32:35 +0300

    Small update

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
 from telethon import TelegramClient, events
+from datetime import datetime
 import config
 
 my_channel_id = -1001442545218
-needed_channel_id = -1001423471875
+channels = [-1001423471875, -1001287887017]
 
 client = TelegramClient('lyp1noff', config.api_id, config.api_hash)
-
 print("Resender started")
 
-@client.on(events.NewMessage(chats=needed_channel_id))
+
+@client.on(events.NewMessage(chats=channels))
 async def my_event_handler(event):
-    if event:
+    chat = await event.get_chat()
+    if event.message.media:
         await client.send_message(my_channel_id, event.message)
-        await print("Reposted")
+        print(datetime.strftime(datetime.now(), "%d.%m.%Y Re-sended at %H:%M:%S"), "from", chat.title)
 
 client.start()
 client.run_until_disconnected()
